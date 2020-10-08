44119bdc895adda75a875240548b42bdf1b0e8b8
CodingNav <navleenkaurr@gmail.com>
2020-10-08 17:04:13 -0400

    Added task creator

--- asset/js/javascript.js
+++ asset/js/javascript.js
@@ -1,5 +1,6 @@
 var taskArray = [];
 var form = document.getElementById("create-task");
+var taskList = document.getElementById("tasks");
 
 
 
@@ -15,5 +16,32 @@ form.onsubmit = function (event) {
     };
     taskArray.push(taskInfo);
     console.log(taskArray);
+    var listTag = document.createElement('li');
+    listTag.classList.add("list-group-item");
+    listTag.innerText = taskTitle;
+
+    if (isComplete) {
+        listTag.classList.add("list-group-item-success");
+    }
+    else {
+        listTag.classList.add("list-group-item-warning");
+    }
+
+    var trashButton = document.createElement("button");
+    trashButton.classList.add("float-right");
+    trashButton.innerHTML = `<i class="fas fa-trash-alt"></i>`;
+    listTag.appendChild(trashButton);
+
+    if (!isComplete) {
+        var checkButton = document.createElement("button");
+        checkButton.classList.add("float-right");
+        checkButton.innerHTML = `<i class="fas fa-clipboard-check"></i>`;
+        listTag.appendChild(checkButton);
+    }
+
+    //<li class="list-group-item list-group-item-success">A simple success list group item</li>
+    taskList.appendChild(listTag);
+
+
 }
 
